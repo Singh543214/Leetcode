e986cd84762c2ba905b92d29a8ac77e43fe629ce
Prince Kumar Singh <155215638+Singh543214@users.noreply.github.com>
2025-12-02 15:40:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         if((moves.length())%2==1) return false;
-        int countUp=0,countRight=0;
+        int countUp=0;
         for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='U') countUp++;
-            else if(moves.charAt(i)=='D') countUp--;
-            else if(moves.charAt(i)=='R') countRight++;
-            else countRight--;
+            if(moves.charAt(i)=='U' || moves.charAt(i)=='R') countUp++;
+            else countUp--;
         }
-        if(countUp==0 && countRight==0) return true;
+        if(countUp==0) return true;
         return false;
     }
 }
