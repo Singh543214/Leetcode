95661fee4aad52d1e3ec6a54cdcab00086e994dc
Prince Kumar Singh <155215638+Singh543214@users.noreply.github.com>
2025-12-26 15:04:24 +0530

    Time: 15 ms (17.76%), Space: 46.9 MB (63.62%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -2,11 +2,9 @@ class Solution {
     public int bestClosingTime(String customers) {
        int n= customers.length();
        int[] open=new int[n+1];
-       int[] close=new int[n+1];
  
        for(int i=0;i<n;i++){
         if(customers.charAt(i)=='Y') open[i]=1;
-        else close[i+1]=1;
        }
 
        for(int i=n-1;i>=0;i--){
@@ -14,22 +12,18 @@ class Solution {
        }
 
        int min=Integer.MAX_VALUE;
-       min=Math.min(min,(open[0]+close[0]));
+       int d=0;
 
-       for(int i=1;i<=n;i++){
-        close[i]+=close[i-1];
-        min=Math.min(min,(open[i]+close[i]));
-       }
-
-       
-
-    //    for(int i=0;i<=n;i++){
-    //     min=Math.min(min,(open[i]+close[i]));
-    //     System.out.println(open[i]+" "+close[i]);
-    //    }
        for(int i=0;i<=n;i++){
-        if(open[i]+close[i]==min) return i;
+        min=Math.min(min,(open[i]+d));
+        d=(i<n && customers.charAt(i)=='N')?(d+1):d;
        }
+       d=0;
+       for(int i=0;i<=n;i++){
+        if(open[i]+d==min) return i;
+        d=(i<n && customers.charAt(i)=='N')?(d+1):d;
+       }
+
        return 0;
     }
 }
