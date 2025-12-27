560e74197181d82a1036d3a80c526c9d21a27854
Prince Kumar Singh <155215638+Singh543214@users.noreply.github.com>
2025-12-27 15:19:21 +0530

    Time: 2 ms (45.99%), Space: 48.3 MB (91.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,12 +3,10 @@ class Solution {
         int count=-1;
         boolean odd=true;
         int total=(nums1.length+nums2.length);
-        if(total%2==0){
-            odd=false;
-            total=total/2;
-        }else total=total/2;
-        System.out.println(total);
-        int x=0,y=0;
+        if(total%2==0)  odd=false;
+        total=total/2;
+        //System.out.println(total);
+        int x=10000000,y=10000000;
         int i=0,j=0;
         while(i<nums1.length && j<nums2.length){
             if(nums1[i]<=nums2[j]){
@@ -23,18 +21,21 @@ class Solution {
                 if(count==total) y=nums2[j];
                 j++;
             }
+            if(x!=10000000 && y!=10000000) break;
         }
         while(i<nums1.length){
             count++;
                 if(count==total-1) x=nums1[i];
                 if(count==total) y=nums1[i];
                 i++;
+                if(x!=10000000 && y!=10000000) break;
         }
         while(j<nums2.length){
                 count++;
                 if(count==total-1) x=nums2[j];
                 if(count==total) y=nums2[j];
                 j++;
+                if(x!=10000000 && y!=10000000) break;
         }
 
         return (odd)?(double)y:(double)(x+y)/(double)2;
